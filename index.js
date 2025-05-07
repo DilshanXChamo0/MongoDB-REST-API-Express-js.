5fe4f55cb76ddb73a79cf91e0cbe18d265b7170a
Chamod Dilshan <chamoddilshan834@gmail.com>
2025-05-07 19:41:59 +0530

    Exprss & MongoDB using build a simple Rest API Create student & Get Student

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+// Models
+const Student = require('./model/Student');
+
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect(process.env.MONGODB_URI)
+     .then(() => { console.log('MongoDB connected') })
+     .catch(err => { console.log(err) });
+
+app.post('/api/v1/student/save', (req, res) => {
+
+     const { name, age, email } = req.body;
+     const student = new Student({ name, age, email });
+     student.save()
+          .then(() => {
+               res.status(200).json({ message: 'Student saved successfully' });
+          })
+          .catch(err => {
+               res.status(500).json({ message: 'Error saving student', error: err });
+          });
+});
+
+app.get('/api/v1/student/:email', (req, res) => {
+
+     const { email } = req.params;
+     Student.findOne({ email }).then((student) => {
+          if (student) {
+               res.status(200).json(student);
+
+          } else {
+               res.status(404).json({ message: 'Student not found' });
+          }
+     }).catch(error => {
+          res.status(500).json({ message: 'Error fetching student', error });
+     })
+});
+
+
+app.listen(process.env.PORT, () => {
+     console.log(`Server is running on port ${process.env.PORT}`);
+});
